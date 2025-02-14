52f065a6bca349915ebe0355c2e930e67c35e87d
TagzHD <grecozank@gmail.com>
2025-02-14 10:59:38 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#import required dependicies
+import discord
+from discord.ext import commands
+
+#import Bot Token
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+
+@client.event
+async def on_ready ():
+    print("Pot Commands")
+    print("---------")
+
+@client.command()
+async def rank(ctx):
+    await ctx.send("Your current rank is Radiant")
+
+
+client.run(os.getenv("MY_KEY"))
+
+
+
+
